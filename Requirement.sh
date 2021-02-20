d7c266cb369f5f0616bb7bc8c28389a8f12a9a6e
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2021-02-20 15:23:29 +0530

    Update Requirement.sh

--- Requirement.sh
+++ Requirement.sh
@@ -11,7 +11,12 @@ apt install nano -y
 apt install ruby -y
 gem install lolcat
 figlet -f big FINISHED !!! | lolcat
-echo
+echo ""
+sleep 10
+echo " create account on cashkaro.com "
+echo " set your browser "
+termux-open-url https://cashkaro.com?r=8026255&fname=Rixon
+echo ""
 echo -e "\e[1m Now Run bash Evil-I.sh...!!!"
 echo
 echo -e "\e[1m\e[32m Developed by :\e[33m Tech Code(Rixon Xavier)"
